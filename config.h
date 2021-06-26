61ba3a03cad23763eea19f9bb776e654720cd474
sam-barr <mail@samf.bar>
2021-06-26 12:03:15 -0400

    added stuff

---
+++ config.h
@@ -0,0 +1,30 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Source Code Pro:dpi=336:size=9"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#6B7089", "#161821" },
+        [SchemeSel] = { "#161821", "#6B7089" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemePrompt] = { "#D2D4DE", "#161821" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 15;
+static unsigned int xoffset    = 1280;
+static unsigned int yoffset    = 720;
+static unsigned int width      = 1280;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 6;
